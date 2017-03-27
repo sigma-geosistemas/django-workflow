ac3577b12d2eb5e6190502a1667fc13bc46db023
Claudio Santos <contato@simplologia.com.br>
2017-03-27 08:52:18 -0300

    inclusão do django-reversion que já estava sendo referenciado

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-workflow-fsm',
-    version='1.0.0',
+    version='1.0.2',
 
     description='Maquina de Estados, navega entre estados, executando tasks e habilitando ações a cada estado',
     long_description='Uma Maquina de Estados configurável, que permite navegar entre seus status, disparar tarefas em sua transição e habilitar ações em cada status.',
@@ -19,6 +19,7 @@ setup(
         "django-phonenumber-field",
         "djangorestframework",
         "djangorestframework-filters",
+        "django-reversion",
         "redis",
     ],
     packages=find_packages(),
